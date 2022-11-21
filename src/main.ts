55f6325fb143f48b382e2b20853dab856a868fa1
Marnix Dessing <m.dessing@nijmegen.nl>
2022-11-21 14:40:37 +0100

    feat: dummy project

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,72 @@
-import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { App, Stack, StackProps, Stage } from 'aws-cdk-lib';
+import { aws_s3 as s3, pipelines as cdkpipelines } from 'aws-cdk-lib'
 import { Construct } from 'constructs';
+
+
+const deploymentAccount = {
+  account: '418648875085', // gemeentenijmegen-deployment
+  region: 'eu-west-1',
+};
+
+const sandbox = {
+  account: '122467643252', // Sandbox
+  region: 'eu-west-1',
+};
+
+
+
+
 
 export class MyStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
-
-    // define resources here...
+    new s3.Bucket(this, 'my-bucket-to-be-retained');
   }
 }
 
-// for development, use account/region from cdk cli
-const devEnv = {
-  account: process.env.CDK_DEFAULT_ACCOUNT,
-  region: process.env.CDK_DEFAULT_REGION,
-};
+export class MyStage extends Stage {
+  constructor(scope: Construct, id: string, props: StackProps = {}) {
+    super(scope, id, props);
+    new MyStack(this, 'my-stack', {
+      env: sandbox
+    });
+  }
+}
+
+const codeStarConnectionArn: string = 'arn:aws:codestar-connections:eu-west-1:418648875085:connection/4f647929-c982-4f30-94f4-24ff7dbf9766';
+
+
+export class MyPipeline extends Stack {
+
+  constructor(scope: Construct, id: string, props: StackProps = {}) {
+    super(scope, id)
+
+    const repository = cdkpipelines.CodePipelineSource.connection('GemeenteNijmegen/webformulieren-dummy', 'main', {
+      connectionArn: codeStarConnectionArn,
+    });
+
+    const pipeline = new cdkpipelines.CodePipeline(this, 'pipeline', {
+      pipelineName: `eform-and-formio-pipeline-main`,
+      crossAccountKeys: true,
+      synth: new cdkpipelines.ShellStep('Synth', {
+        input: repository,
+        commands: [
+          'yarn install --frozen-lockfile', //nodig om projen geinstalleerd te krijgen
+          'yarn build',
+        ],
+      }),
+    });
+
+
+    pipeline.addStage(new MyStage(this, 'webformulieren-dummy-my-stage'))
+
+  }
+
+
+}
 
 const app = new App();
 
-new MyStack(app, 'webformulierne-dummy-project-dev', { env: devEnv });
-// new MyStack(app, 'webformulierne-dummy-project-prod', { env: prodEnv });
+new MyPipeline(app, 'webformulierne-dummy-project-pipeline', { env: deploymentAccount });
 
 app.synth();
